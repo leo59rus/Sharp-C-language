0c12f9dffe08b92afc1fb8c435262304b91e1594
leonid <orange59usa@gmail.com>
2022-12-18 16:22:12 +0300

    4я задача

---
+++ homeWork1/example04/Program.cs
@@ -0,0 +1,19 @@
+﻿ Console.Write("введите число, программа выдаст все четные числа от одного до Вашего числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+if (a < 2) {
+    Console.WriteLine("нет чётных чисел");
+} else 
+{
+    
+    while(counter < (a - 1)){
+        counter = counter + 2;
+      Console.Write(counter + " ");  
+    }
+}
+
+//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
