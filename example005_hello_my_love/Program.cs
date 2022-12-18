9f715020159edddea2a97daaec987af961d71a06
leonid <orange59usa@gmail.com>
2022-12-18 12:03:07 +0300

    hello my love

---
+++ example005_hello_my_love/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("введите своё имя - ");
+string userName = Console.ReadLine();
+if(userName.ToLower == "Мария")//
+{
+    Console.WriteLine("Привет, моя любовь");
+}
+else{
+    Console.WriteLine("Привет, " + userName);
+}
