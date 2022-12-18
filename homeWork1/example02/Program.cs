daa1e0ec07972b2f3c980123d290a0517ce7cbe3
leonid <orange59usa@gmail.com>
2022-12-18 16:00:17 +0300

    решение 2й задачи

---
+++ homeWork1/example02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("введите число а ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число c ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine(max);
+
+//Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
