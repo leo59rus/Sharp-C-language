23d55d0ddb0e0a2922b55befdfdac0deedd0d029
leonid <orange59usa@gmail.com>
2022-12-29 23:42:38 +0300

    1я задача

--- homeWork1/example03/Program.cs
+++ homeWork1/example03/Program.cs
@@ -1,4 +1,4 @@
-﻿ Console.Write("введите число, программа определит четное оно или нет ");
+﻿Console.Write("введите число, программа определит четное оно или нет ");
 int a = Convert.ToInt32(Console.ReadLine());
 if ((a%2) ==0 ){
     Console.WriteLine("да");
