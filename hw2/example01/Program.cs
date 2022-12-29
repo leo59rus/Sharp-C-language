7fa385bba85428531f413dbb78d9a8f2d179dc7b
leonid <orange59usa@gmail.com>
2022-12-30 00:41:03 +0300

    решение 2 домашки

---
+++ hw2/example01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("введи трёхзначное число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number);
+int hundred = number/100;
+//Console.WriteLine(hundred);
+int d = (number - hundred*100);//убрали сотни
+//Console.WriteLine(d);
+int result = d/10;//узнали колво десятков
+
+Console.WriteLine("второя цифра твоего числа - " + result);
---
+++ hw2/example02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("введи число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = number / 100;//трехзначное число?
+if (d<1){
+    Console.WriteLine("третьей цифры нет"); 
+}
+
+if (d<10 && d>=1 ){
+Console.WriteLine("трехзначное число");
+int hundred = number/100;
+int c = (number - hundred*100);
+int result = c%10;
+Console.WriteLine("третья цифра - " + result);
+}
+
+if (d<100 && d>=10 ){
+    Console.WriteLine("четырехзначное число");
+   
+}
+
+if (d<1000 && d>=100 ){
+    Console.WriteLine("пятизначное число");
+    int c1 = number/10000;
+    int c2 = number - c1*10000;//стало четырехзначное
+    int b1 = c2/1000;
+    int b2 = c2 - b1*1000;//сталотрёхзначное
+    Console.WriteLine("третья цифра - " + b2/100);
+}
